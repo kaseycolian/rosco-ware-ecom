06697a9b46b0fa50e4f82042d3ee74a60ea181d0
BenWilliams <deanpbr@gmail.com>
2018-03-23 15:34:14 -0400

    Catalog mapping tests started

--- src/main/java/com/roscoware/ecom/catalog/Product.java
+++ src/main/java/com/roscoware/ecom/catalog/Product.java
@@ -1,13 +1,22 @@
 package com.roscoware.ecom.catalog;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 @Entity
 public class Product {
+
 	@Id
+	@GeneratedValue
 	private long id;
 	private String name;
+	private String type;
+	private String description;
+	private double price;
+	@ManyToOne
+	private Category category;
 
 	public Product(String name) {
 		this.name = name;
@@ -17,11 +26,57 @@ public class Product {
 	private Product() {
 	}
 
+	public Product(String name, String type, String description, double price, Category category) {
+		this.name = name;
+		this.type = type;
+		this.description = description;
+		this.price = price;
+		this.category = category;
+
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public Category getCategory() {
+		return category;
+	}
+
 	public long getId() {
 		return id;
 	}
 
 	public String getName() {
 		return name;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null || getClass() != obj.getClass()) {
+			return false;
+		}
+		return id == ((Product) obj).id;
+	}
+
+	@Override
+	public int hashCode() {
+		return ((Long) id).hashCode();
+	}
+
+	@Override
+	public String toString() {
+		return name + " | " + category.getName();
 	}
 }
