b8074ca8763992235e698d7f25139e6a1abc50e7
Kasey <KaseyColian@gmail.com>
2018-03-24 17:13:29 -0400

    working on adding and removing items from cart. need to create tests to allow objects to be added and removed. then working on adding total cost of items in cart.

--- src/main/java/com/roscoware/ecom/catalog/Cart.java
+++ src/main/java/com/roscoware/ecom/catalog/Cart.java
@@ -10,32 +10,47 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToMany;
 
+//Kasey is going to work on adding and removing items from the cart, then calculating the total price of the cart.
+
 @Entity
 public class Cart {
-	
+
 	@Id
 	@GeneratedValue
 	private Long id;
 
-	@ManyToMany 
-	private Collection<Product> products;
-	
-	public Collection<Product> getProducts(){
+	@ManyToMany
+	private HashSet<Product> products;
+
+	public HashSet<Product> getProducts() {
 		return products;
 	}
 
 	public Cart(Product... products) {
-	this.products = new HashSet<>(asList(products));
+		this.products = new HashSet<Product>(asList(products));
 	}
 
 	public Long getId() {
 		return id;
 	}
-	
+
+	public void addProductToCart(Product product) {
+		products.add(product);
+
+	}
+
+	public void removeProductFromCart(Product product) {
+		products.remove(product);
+	}
+
+	public void getPriceTotalOfCart() {
+		for (products)
+	}
+
 	@SuppressWarnings("unused")
 	private Cart() {
 	}
-	
+
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj) {
@@ -51,7 +66,5 @@ public class Cart {
 	public int hashCode() {
 		return ((Long) id).hashCode();
 	}
-
-	
 
 }
