f5cb513f1a8d5870471b017f8a20f7845981a836
Kasey <KaseyColian@gmail.com>
2018-03-24 13:47:31 -0400

    1st Cart Mapping Test Passing - able to retrieve many products in 1 cart

--- src/test/java/com/roscoware/ecom/catalog/CartMappingTest.java
+++ src/test/java/com/roscoware/ecom/catalog/CartMappingTest.java
@@ -1,17 +1,30 @@
 package com.roscoware.ecom.catalog;
 
-import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.Assert.assertThat;
 
 import javax.annotation.Resource;
 
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
+//Tests:
+//shouldRetrieveCartById
+//should save Many Products in one cart
+//should have Many Carts with same product
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@DataJpaTest
 public class CartMappingTest {
+
 	@Resource
 	private TestEntityManager entityManager;
+
+	@Resource
+	private ProductRepository productRepo;
 
 	@Resource
 	private CategoryRepository categoryRepo;
@@ -19,21 +32,44 @@ public class CartMappingTest {
 	@Resource
 	private CartRepository cartRepo;
 
+	// @Test
+	// public void shouldSaveAndLoadACartFromDbById() {
+
+	// Long underTest = testCart.getId();
+	//
+	// entityManager.flush();
+	// entityManager.clear();
+	//
+	// testCart = cartRepo.findOne(underTest);
+	//
+	// assertThat(testCart.getId(), is(underTest));
+	// }
+
+	// @Test
+	// public void
+
 	@Test
-	public void shouldSaveAndLoadACartFromDb() {
+	public void shouldSaveTwoProductsToOneCart() {
 		Category testCategory = new Category("cat", "so cool");
 		testCategory = categoryRepo.save(testCategory);
-		Product product1 = new Product("", "", "", 5, testCategory);
-		Cart testCart = new Cart(product1);
 
-		Long underTest = testCart.getId();
+		Product product1 = new Product("", "", "", 5, testCategory, "");
+		product1 = productRepo.save(product1);
+		Product product2 = new Product("", "", "", 4, testCategory, "");
+		product2 = productRepo.save(product2);
+
+		Cart testCart = new Cart(product1, product2);
+		testCart = cartRepo.save(testCart);
+
+		long testCartId = testCart.getId();
 
 		entityManager.flush();
 		entityManager.clear();
 
-		testCart = cartRepo.findOne(underTest);
+		testCart = cartRepo.findOne(testCartId);
 
-		assertThat(testCart.getId(), is(underTest));
+		assertThat(testCart.getProducts(), containsInAnyOrder(product1, product2));
 
 	}
+
 }
