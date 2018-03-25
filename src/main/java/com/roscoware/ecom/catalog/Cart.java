297a786b3ed26cd777f4e5206c03bd2e9691023f
Kasey <KaseyColian@gmail.com>
2018-03-25 11:57:51 -0400

    Added customerName to Cart constructor. All tests for Cart passing.

--- src/main/java/com/roscoware/ecom/catalog/Cart.java
+++ src/main/java/com/roscoware/ecom/catalog/Cart.java
@@ -5,31 +5,40 @@ import javax.persistence.Id;
 
 //Adding an innerClass between Cart & Product called CartItem which will be mapped to Product instead of Cart
 
-
 public class Cart {
 
 	@Id
 	@GeneratedValue
 	private Long id;
+	private String customerName;
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public static double getDefaultCartbalance() {
+		return DEFAULT_CARTBALANCE;
+	}
 
 	static final double DEFAULT_CARTBALANCE = 0.00;
 	protected double cartBalance = DEFAULT_CARTBALANCE;
 
-	public Cart(double cartBalance) {
-		this.cartBalance = cartBalance;
+	public Cart(double cartTotal, String customerName) {
+		this.cartBalance = cartTotal;
+		this.customerName = customerName;
 	}
 
 	public Long getId() {
 		return id;
 	}
 
-	public double getCartBalance() {
+	public double getCartTotal() {
 		return cartBalance;
 	}
 
-//	@SuppressWarnings("unused")
-//	private Cart() {
-//	}
+	// @SuppressWarnings("unused")
+	// private Cart() {
+	// }
 
 	@Override
 	public boolean equals(Object obj) {
--- src/test/java/com/roscoware/ecom/catalog/CartTest.java
+++ src/test/java/com/roscoware/ecom/catalog/CartTest.java
@@ -9,34 +9,19 @@ import org.junit.Test;
 
 public class CartTest {
 
-	
-	@Resource
-	private CartRepository cartRepo;
+	// Category testCategory = new Category("cat", "is cool");
+	// Product product1 = new Product("", "", "", 2.00, testCategory, "");
+	// Product product2 = new Product("", "", "", 3.00, testCategory, "");
+	Cart testCart = new Cart(1, "Mister Anderson");
 
-	@Resource
-	private CategoryRepository categoryRepo;
-
-	@Resource
-	private ProductRepository productRepo;
-	
-	Category testCategory = new Category("cat", "is cool");
-	Product product1 = new Product("", "", "", 2.00, testCategory, "");
-	Product product2 = new Product("", "", "", 3.00, testCategory, "");
-	Cart testCart = new Cart(1, product1);
-
-	
 	@Test
 	public void shouldReturnCartBalance() {
-		assertThat(testCart.getCartBalance(), is(1.00));
+		assertThat(testCart.getCartTotal(), is(1.00));
 	}
 
-	
-//	@Test
-//	public void shouldGetTotalNumberOfItemsInCart() {
-//		int originalQuantityOfProducts = testCart.getProducts().size();
-//		testCart.addProductToCart(product2);
-//		int updatedQuantityOfProducts = testCart.getProducts().size();
-//		assertThat(updatedQuantityOfProducts-originalQuantityOfProducts, is(1));
-//	}
+	@Test
+	public void shouldReturnCustomerName() {
+		assertThat(testCart.getCustomerName(), is("Mister Anderson"));
+	}
 
 }
