d13f4611a20938a62299d810812ae8368de13835
BenWilliams <deanpbr@gmail.com>
2018-03-25 18:26:31 -0400

    Sunday merge with Rochelle

--- src/main/java/com/roscoware/ecom/RoscoWareEcomApplication.java
+++ src/main/java/com/roscoware/ecom/RoscoWareEcomApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EcomApplication {
+public class RoscoWareEcomApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(EcomApplication.class, args);
+		SpringApplication.run(RoscoWareEcomApplication.class, args);
 	}
 }
