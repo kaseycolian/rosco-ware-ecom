58fe9eb47313a4dd63b395918c4aea339c01befe
Kasey <KaseyColian@gmail.com>
2018-03-26 08:22:22 -0400

    ALL tests passing!

--- src/test/java/com/roscoware/ecom/catalog/CatalogMvcTest.java
+++ src/test/java/com/roscoware/ecom/catalog/CatalogMvcTest.java
@@ -14,8 +14,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 @RunWith(SpringRunner.class)
-@WebMvcTest
-
+@WebMvcTest(BrowseController.class)
 public class CatalogMvcTest {
 	@Resource
 	MockMvc mvc;
