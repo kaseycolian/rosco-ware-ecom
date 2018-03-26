857a2bc7a437985248de587ba88d9287558c4f02
BenWilliams <deanpbr@gmail.com>
2018-03-26 09:40:47 -0400

    merge

--- src/main/java/com/roscoware/ecom/catalog/ShoppingCartController.java
+++ src/main/java/com/roscoware/ecom/catalog/ShoppingCartController.java
@@ -25,8 +25,13 @@ public class ShoppingCartController {
 	@RequestMapping("/shoppingCarts/{cartId}")
 	public ShoppingCart findShoppingCart(@PathVariable(name = "cartId") long cartId) {
 		ShoppingCart selectedCart = shoppingCartRepo.findOne(cartId);
-		
+
 		return selectedCart;
 	}
-		
+
+	@RequestMapping("/add-to-shoppingCart/{cartId}")
+	public void addToShoppingCart(@PathVariable(name = "cartId") long cartId) {
+
+	}
+
 }
