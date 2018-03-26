53573b6a6effbc34437d217ab3f167048da9b490
BenWilliams <deanpbr@gmail.com>
2018-03-26 00:14:22 -0400

    Added correct image urls

--- src/main/java/com/roscoware/ecom/catalog/CatalogPopulator.java
+++ src/main/java/com/roscoware/ecom/catalog/CatalogPopulator.java
@@ -23,17 +23,17 @@ public class CatalogPopulator implements CommandLineRunner {
 		clothing = categoryRepo.save(clothing);
 		accessories = categoryRepo.save(accessories);
 		Product roscoShorts = new Product("Rosco Shorts", "Shorts", "This is our original short product.", 2.25,
-				clothing, "./images/sample1.jpg");
+				clothing, "../images/dogshorts.jpg");
 		Product roscoPants = new Product("Rosco Pants", "Pants", "This is the pants version of our shorts.", 4.50,
-				clothing, "./images/sample1.jpg");
+				clothing, "../images/dogpants.jpg");
 		roscoShorts = productRepo.save(roscoShorts);
 		roscoPants = productRepo.save(roscoPants);
 
 		Product roscoCollar = new Product("Rosco Collar", "Collars",
 				"This is a bejeweled collar that bedazzles all the dogs around.", 12.50, accessories,
-				"./images/sample1.jpg");
+				"../images/dogcollar.jpg");
 		Product roscoLeash = new Product("Rosco Leash", "Leashes", "A leash fit for a royal dog at a peasant price.",
-				8.75, accessories, "./images/sample1.jpg");
+				8.75, accessories, "../images/dogleash.jpg");
 		roscoCollar = productRepo.save(roscoCollar);
 		roscoLeash = productRepo.save(roscoLeash);
 		ShoppingCart cart = new ShoppingCart();
