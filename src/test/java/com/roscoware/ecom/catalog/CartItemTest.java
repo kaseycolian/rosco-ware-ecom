ec4877453af2557969a0f51950fa872f6105171e
Kasey <KaseyColian@gmail.com>
2018-03-25 23:39:29 -0400

    cartItem passing. going to merge upstream

--- src/test/java/com/roscoware/ecom/catalog/CartItemTest.java
+++ src/test/java/com/roscoware/ecom/catalog/CartItemTest.java
@@ -1,5 +1,20 @@
 package com.roscoware.ecom.catalog;
 
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+import org.junit.Test;
+
 public class CartItemTest {
 
+	@Test
+	public void shouldReturnQuantityOfIndividualProdudct() {
+		Category testCategory = new Category ("stuff", "fun");
+		Product testProduct = new Product ("", "", "", 5.00, testCategory, "");
+		ShoppingCart testCart = new ShoppingCart();
+		CartItem underTest = new CartItem(testProduct, 2, testCart);
+		int itemQuantity =  underTest.getQuantityOfIndividualProduct();
+		assertThat(itemQuantity, is(2));
+	}
+	
 }
